e4906c9bcdc2c46175db1937028e5a9d91abd6b6
Jakub Oskera <iam@jakuboskera.dev>
2021-10-04 20:41:03 +0200

    feat(terraform-cloud): Add VCS root

--- terraform-cloud/main.tf
+++ terraform-cloud/main.tf
@@ -3,7 +3,26 @@ resource "tfe_organization" "jakuboskera" {
   email = "iam@jakuboskera.dev"
 }
 
+resource "tfe_workspace" "terraform-cloud" {
+  name              = "terraform-cloud"
+  organization      = tfe_organization.jakuboskera.id
+  working_directory = "terraform-cloud"
+  vcs_repo {
+    branch             = "main"
+    identifier         = "jakuboskera/infra"
+    ingress_submodules = false
+    oauth_token_id     = ""
+  }
+}
+
 resource "tfe_workspace" "heroku" {
-  name         = "heroku"
-  organization = tfe_organization.jakuboskera.id
+  name              = "heroku"
+  organization      = tfe_organization.jakuboskera.id
+  working_directory = "heroku"
+  vcs_repo {
+    branch             = "main"
+    identifier         = "jakuboskera/infra"
+    ingress_submodules = false
+    oauth_token_id     = ""
+  }
 }
