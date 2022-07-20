5d0573ea1a65cf82794cae38a41c193c8db7735e
Jakub Oskera <iam@jakuboskera.dev>
2022-07-20 11:08:12 +0200

    feat(heroku): add app should-be-done

--- heroku/main.tf
+++ heroku/main.tf
@@ -14,6 +14,26 @@ resource "heroku_domain" "write-comment" {
   hostname = "guestbook.jakuboskera.dev"
 }
 
+resource "heroku_app" "should-be-done" {
+  name   = "should-be-done"
+  region = "eu"
+  acm    = false
+}
+
+resource "heroku_addon" "should-be-done" {
+  app  = heroku_app.should-be-done.name
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_domain" "should-be-done" {
+  app      = heroku_app.should-be-done.name
+  hostname = "todo.jakuboskera.dev"
+}
+
 output "write-comment_url" {
   value = "https://${heroku_app.write-comment.name}.herokuapp.com"
+}
+
+output "should-be-done_url" {
+  value = "https://${heroku_app.should-be-done.name}.herokuapp.com"
 }
