d401e571e43f9e57ec597f43691322f466aec60c
Jakub Oskera <iam@jakuboskera.dev>
2023-03-07 08:18:34 +0100

    fix: remove Heroku workspace from terraform cloud

--- terraform-cloud/terraform-cloud.tf
+++ terraform-cloud/terraform-cloud.tf
@@ -5,26 +5,8 @@ resource "tfe_workspace" "terraform-cloud" {
 }
 
 # These vars are needed to be part of terraform-cloud workspace in
-# order create these vars in other workspaces like cloudflare, heroku etc.
+# order create these vars in other workspaces like cloudflare, etc.
 # and then used by this workspaces
-resource "tfe_variable" "terraform-cloud-heroku_email" {
-  key          = "heroku_email"
-  value        = var.heroku_email
-  category     = "terraform"
-  sensitive    = true
-  workspace_id = tfe_workspace.terraform-cloud.id
-  description  = "Heroku email"
-}
-
-resource "tfe_variable" "terraform-cloud-heroku_api_key" {
-  key          = "heroku_api_key"
-  value        = var.heroku_api_key
-  category     = "terraform"
-  sensitive    = true
-  workspace_id = tfe_workspace.terraform-cloud.id
-  description  = "Heroku api key"
-}
-
 resource "tfe_variable" "terraform-cloud-tfe_token" {
   key          = "tfe_token"
   value        = var.tfe_token
@@ -59,15 +41,6 @@ resource "tfe_variable" "terraform-cloud-icloud_dns_token" {
   sensitive    = true
   workspace_id = tfe_workspace.terraform-cloud.id
   description  = "iCloud DNS token"
-}
-
-resource "tfe_variable" "terraform-cloud-heroku_should-be-done_api_key" {
-  key          = "heroku_should-be-done_api_key"
-  value        = var.heroku_should-be-done_api_key
-  category     = "terraform"
-  sensitive    = true
-  workspace_id = tfe_workspace.terraform-cloud.id
-  description  = "API key for https://todo.jakuboskera.dev application"
 }
 
 resource "tfe_variable" "terraform-cloud-todo_api_key" {
